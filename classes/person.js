36c1250a83642b00b20e2ac1deb579717cdef011
beka-tech <abeka039@gmail.com>
2025-11-20 12:32:52 +0300

    Class Syntax
    Class Syntax

--- classes/person.js
+++ classes/person.js
@@ -1,7 +1,31 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 
-/****************************************************************************/
-/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+  introduce() {
+    console.log(
+      `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`
+    );
+  }
+
+  static introducePeople(peopleArray) {
+    if (!Array.isArray(peopleArray)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    for (let person of peopleArray) {
+      if (!(person instanceof Person)) {
+        throw new Error("All items in array must be Person class instances.");
+      }
+    }
+
+    // call introduce() on each person
+    peopleArray.forEach((p) => p.introduce());
+  }
+}
 
 try {
   module.exports = Person;
